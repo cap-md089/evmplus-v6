86c7deaa8c66bc8043a06e7ebec828d194272b02
Andrew Rioux <rioux.andrew.d@gmail.com>
2018-06-15 12:27:37 -0400

    Configuration changes

--- server/src/conf.ts
+++ server/src/conf.ts
@@ -1,10 +1,10 @@
 export const Configuration = {
 	database: {
 		connection: {
-			port: 3306,
+			port: 33389,
 			user: 'em',
-			password: 'alongpassword2016',
-			host: '127.0.0.1',
+			password: 'alongpassword2017',
+			host: '192.168.45.10',
 			database: 'EventManagement4'
 		},
 		connectionCount: 15
