97aee2448f4aee122329da2dada58393a5c96325
Glenn Rioux <grioux.cap@gmail.com>
2018-11-10 19:40:01 -0500

    Working on event linked list

--- client/src/pages/EventLinkList.tsx
+++ client/src/pages/EventLinkList.tsx
@@ -2,9 +2,40 @@ import * as React from 'react';
 import Loader from '../components/Loader';
 import Event from '../lib/Event';
 import { PageProps } from './Page';
+import { Link } from 'react-router-dom';
+import './EventLinkList.css';
+import { EventStatus } from '../enums';
+// import { numberTypeAnnotation } from 'babel-types';
+
 
 interface LinkListState {
 	eventList: Event[] | null;
+}
+
+function getEventStatus(status: RadioReturn<EventStatus>) {
+	switch (status[0]) {
+		case EventStatus.COMPLETE:
+			return <span style={{ color: 'green' }}>Complete</span>;
+		case EventStatus.CANCELLED:
+			return <span style={{ color: 'red' }}>Cancelled</span>;
+		case EventStatus.CONFIRMED:
+			return <span style={{ color: 'blue' }}>Confirmed</span>;
+		case EventStatus.DRAFT:
+			return <span style={{ color: 'orange' }}>Draft</span>;
+		case EventStatus.INFORMATIONONLY:
+			return <span style={{ color: 'blue' }}>Info Only</span>;
+		case EventStatus.TENTATIVE:
+			return <span style={{ color: 'orange' }}>Tentative</span>;
+	}
+	return null;
+}
+
+function getEchelonEventNumber(gen: RadioReturn<number>) {
+	switch (true) {
+		case true:
+			return <span style={{ color: 'blue' }}>Not Req</span>;
+	}
+	return null;
 }
 
 export default class LinkList extends React.Component<
@@ -24,7 +55,8 @@ export default class LinkList extends React.Component<
 			eventList = eventList.sort((a, b) => a.name.localeCompare(b.name));
 
 			eventList = eventList.filter(
-				event => event.startDateTime < ((Date.now() / 1000) + (60*60*24*30))
+				event =>
+					event.startDateTime < Date.now() / 1000 + 60 * 60 * 24 * 30
 			);
 
 			this.setState({ eventList });
@@ -32,17 +64,45 @@ export default class LinkList extends React.Component<
 	}
 
 	public render() {
+		// need to check for sessionid here and return login error if not current
+
 		return this.state.eventList === null ? (
 			<Loader />
 		) : this.state.eventList.length === 0 ? (
 			<div>No events to list</div>
 		) : (
-			<table>
-				<tr><th>Name:</th><th>id:</th></tr>
-				{this.state.eventList.map((event, i) => (
-					<tr key={i}>{event.name}</tr>
+			<div className="eventlinklist">
+				<table>
+					<tr>
+						<th>Event ID :: Name, Start Date</th>
+						<th>Status</th>
+						<th>
+							<span style={{ color: 'green' }}>GP</span>/
+							<span style={{ color: 'gray' }}>WG</span>/
+							<span style={{ color: 'red' }}>RG</span> Event No.
+						</th>
+						<th>Wing Cal</th>
+						<th>Debrief</th>
+					</tr>
+					{this.state.eventList.map((event, i) => (
+						<tr key={i}>
+							<td>
+								{event.accountID}-{event.id} ::{' '}
+								<Link to={`/eventviewer/${event.id}`}>
+									{event.name}
+								</Link>
+								, {event.startDateTime}
+							</td>
+							<td>{getEventStatus(event.status)}</td>
+							<td>
+								{getEchelonEventNumber(event.groupEventNumber)}
+							</td>
+							<td>{event.publishToWingCalendar}</td>
+							<td>{event.debrief}</td>
+						</tr>
 					))}
-			</table>
+				</table>
+			</div>
 		);
 	}
 }
