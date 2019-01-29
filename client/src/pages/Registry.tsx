babcc8bfc44816cecfd04dcf2259935130c6620d
Andrew Rioux <rioux.d.andrew@gmail.com>
2019-01-29 10:11:18 -0500

    Stricter adherance to Page

--- client/src/pages/Registry.tsx
+++ client/src/pages/Registry.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import Page from './Page';
+import Page, { PageProps } from './Page';
 
-export default class RegEdit extends Page {
+export default class RegEdit extends Page<PageProps, {}> {
+	public state: {} = {};
+
 	public render() {
 		return <div />;
 	}
