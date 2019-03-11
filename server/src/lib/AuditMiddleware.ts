79daf18e4f40bd8caa76a7f560c67a71a274b1e0
Andrew Rioux <rioux.d.andrew@gmail.com>
2019-03-11 12:16:42 -0400

    Added audit middleware, need to use it

---
+++ server/src/lib/AuditMiddleware.ts
@@ -0,0 +1,20 @@
+import { AuditLogItem, HTTPRequestMethod } from 'common-lib';
+import { ConditionalMemberRequest } from './members/NHQMember';
+import { asyncErrorHandler } from './Util';
+
+export default asyncErrorHandler((req: ConditionalMemberRequest, res, next) => {
+	const item: AuditLogItem = {
+		accountID: req.account.id,
+		actor: req.member ? req.member.getReference() : { type: 'Null' },
+		method: req.method as HTTPRequestMethod,
+		target: req._originalUrl,
+		timestamp: Date.now()
+	};
+
+	const audits = req.mysqlx.getCollection<AuditLogItem>('Audits');
+
+	// Don't wait for it to finish, it's not necessary
+	audits.add(item).execute();
+
+	next();
+});
