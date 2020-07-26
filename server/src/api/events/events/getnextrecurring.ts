d6f3d6ca47158e2dc9aa3f3e99e047406272c64d
Andrew Rioux <rioux.d.andrew@gmail.com>
2020-07-26 15:10:12 -0400

    Fixed 'Upcoming meeting' always being empty

--- server/src/api/events/events/getnextrecurring.ts
+++ server/src/api/events/events/getnextrecurring.ts
@@ -22,16 +22,14 @@ import { api, asyncRight, errorGenerator, Maybe } from 'common-lib';
 import { collectResults, queryEventsFind } from 'server-common';
 
 const getNextRecurringQuery = queryEventsFind(
-	'activity.values[5].selected = true AND endDateTime > :endDateTime'
+	'activity.values[5] = true AND endDateTime > :endDateTime'
 );
 
 export const func: (now?: () => number) => ServerAPIEndpoint<api.events.events.GetNextRecurring> = (
 	now = Date.now
 ) => req =>
 	asyncRight(
-		getNextRecurringQuery(req.mysqlx)(req.account)({
-			endDateTime: now(),
-		}).limit(1),
+		getNextRecurringQuery(req.mysqlx)(req.account)({ endDateTime: now() }).limit(1),
 		errorGenerator('Could not get event information')
 	)
 		.map(collectResults)
