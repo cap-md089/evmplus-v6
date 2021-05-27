94a44ff763af514a87075025241b98043372bf75
Andrew Rioux <andrew.rioux@mdwg.cap.gov>
2021-05-27 19:27:09 -0400

    fix(client): fixed old reference to capunit
    affects: client
    
    ISSUES CLOSED: #109

--- packages/client/src/lib/apis.ts
+++ packages/client/src/lib/apis.ts
@@ -26,10 +26,9 @@ export default fetchApi;
 export const fetchAPIForAccount = (accountID: string) =>
 	apiGenerator((url: RequestInfo, opts?: RequestInit) => {
 		const newUrl = `${window.location.protocol}//${accountID}.${
-			process.env.NODE_ENV === 'development' ? 'localcapunit' : 'capunit'
-		}.com${process.env.NODE_ENV === 'development' ? ':3001' : ''}${
+			process.env.NODE_ENV === 'development' ? 'localevmplus' : 'evmplus'
+		}.org${process.env.NODE_ENV === 'development' ? ':3001' : ''}${
 			typeof url === 'string' ? (!url.startsWith('/') ? `/${url}` : url) : url.url
 		}`;
-
 		return fetchFunction(newUrl, opts);
 	});
