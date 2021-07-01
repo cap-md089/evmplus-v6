c474d682a68dae6230cd31fc17dd15b20467eac7
Andrew Rioux <arioux@md.cap.gov>
2021-06-30 21:44:04 -0400

    chore: merge

--- packages/server-jest-config/src/conf.ts
+++ packages/server-jest-config/src/conf.ts
@@ -5,7 +5,7 @@ export default (connection: TestConnection): ServerConfiguration => ({
 	AWS_ACCESS_KEY_ID: '',
 	AWS_SECRET_ACCESS_KEY: '',
 	DB_HOST: 'mysql',
-	DB_PASSWORD: 'toor',
+	DB_PASSWORD: 'root',
 	DB_POOL_SIZE: 50,
 	DB_PORT: 33060,
 	DB_SCHEMA: connection.schema,
